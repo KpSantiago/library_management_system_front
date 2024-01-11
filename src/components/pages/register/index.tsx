5c24eeb15b3f4eaa1cd92f81920b519fb71f7b86
KpSantiago <santiago.kauajose11@gmail.com>
2024-01-11 12:35:01 -0300

    o loading component ao register component

--- src/components/pages/register/index.tsx
+++ src/components/pages/register/index.tsx
@@ -1,10 +1,15 @@
+import { Suspense } from "react";
 import LoginRegister from "../../layout/login-register";
 import './style.css';
+import Loading from "../../layout/loading";
 
 export default function Register() {
     return (
-        <div className="container bg-white min-w-full max-w-screen h-screen max-h-screen grid place-items-center">
-            <LoginRegister />
-        </div>
+        <Suspense fallback={<Loading bg='#010101' size='screen'/>}>
+            <div className="container bg-white min-w-full max-w-screen h-screen max-h-screen grid place-items-center">
+                <LoginRegister />
+            </div>
+        </Suspense>
+
     )
 }
